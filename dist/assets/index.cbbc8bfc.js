1919c10dfe96907ba2da2552c9dcd3683f3bbbb7
uvite <buniwota@gmail.com>
2023-04-14 13:32:41 +0800

    add vm

---
+++ dist/assets/index.cbbc8bfc.js
@@ -0,0 +1,1 @@
+import{_,z as o,r as n,d as s,o as t,c,e as a}from"./index.b397180e.js";const d={class:"app-container home"},r=o({name:"Index"}),p=Object.assign(r,{setup(i){return n("3.8.5"),(l,m)=>{const e=s("el-divider");return t(),c("div",d,[a(e)])}}}),u=_(p,[["__scopeId","data-v-03ec221d"]]);export{u as default};
